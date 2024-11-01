cb8d36b9510f25b3c1e04742e1d2cbd6043941d6
AcidNick <nickolaylauvik@gmail.com>
2024-11-01 14:00:09 +0100

    Update

---
+++ Lab6/fantasy_game_inventory.py
@@ -0,0 +1,11 @@
+def display_inventory(inventory: dict) -> str:
+    inventory_str = 'Inventory:\n'
+    for key, value in inventory.items():
+        inventory_str += f'{value} {key}\n'
+    total_items = sum(inventory.values())
+    inventory_str += f'\nTotal number of items: {total_items}'
+    return inventory_str
+
+
+stuff = {"rope": 1, "torch": 6, "gold coin": 42, "dagger": 1, "arrow": 12}
+print(display_inventory(stuff))
